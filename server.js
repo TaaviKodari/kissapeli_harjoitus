68f61f18a346da7afce4761372e5f058a9cf2a2f
Mutti88 <juomakuski@hotmail.com>
2020-03-08 13:33:50 +0200

    heroku tsydeemni  aloitettu

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const app = express();
-app.listen(3000,()=> console.log("kuuntelen"));
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=> console.log("kuuntelen" + port));
 app.use(express.static("public"));
+
+app.use(express.json({limit: '1mb'}));
+
+cons pisteet = [{
+  "pelaaja" : "Taavi",
+  "pisteet" : "1"
+},
+"pelaaja" : "John Doe",
+"pisteet" : "0"
+]
+app.get('/api/pisteet', function(request,respons))
+{
+  respons.send(pisteet);
+})
